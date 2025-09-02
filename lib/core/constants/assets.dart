f3027b9135ebbd1796158f875411966f3629c06d
FatmaIsmail101 <ismailfatma197@gmail.com>
2025-09-02 21:42:39 +0300

    Screen Utils Usage

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -11,4 +11,16 @@ class AppAssets {
   static const String image7 = "assets/images/img7.png";
   static const String image8 = "assets/images/img8.png";
   static const String image9 = "assets/images/imag9.png";
+  static const String mailIcn = 'assets/icons/mail_icn.png';
+  static const String passwordIcn = 'assets/icons/password_icn.png';
+  static const String phoneIcn = 'assets/icons/phone_icn.png';
+  static const String eyeOff = 'assets/icons/eye off_.png'; //
+  static const String identificationIcn = 'assets/icons/Identification_icn.png';
+  static const String englishIcn = 'assets/icons/english.png';
+  static const String arabicIcn = 'assets/icons/arabic.png';
+
+  static const String boyAvatar = 'assets/images/boy_avatar.png';
+  static const String coolMan = 'assets/images/cool_man.png';
+  static const String matureMan = 'assets/images/mature_man.png';
+  static const String women = 'assets/images/women.png';
 }
